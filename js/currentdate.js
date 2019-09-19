9dd683065b82ba65bc4cccc3eb9eef41987258b6
guevaracastroandres <cas18057@byui.edu>
2019-09-19 16:14:08 -0500

    knkknkn

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,2 +1,32 @@
-const options = {wekday: 'long', day: 'numeric' , month: 'long', year: 'numeric'};
-document.getElementBy('currentdate').textContent = new Date().toLocaleDateString('en-us', option);
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Aaturday",
+]
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Agust",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+const todasydate = new Date();
+const dayName = dayName[todasydate.getDay()];
+const monthName = months [getMonth()];
+const year = todasydate.getFullYear;
+const currentdate = dayName + ", "  +  todasydate.getDate() + " " + monthName +", " +todasydate.getFullYear();
+
+document.getElementBy('currentdate').textContent = currentdate;
