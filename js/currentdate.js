c512f71f585932f6add64c5bb5a52d84788d2cd6
guevaracastroandres <cas18057@byui.edu>
2019-09-21 03:05:16 -0500

    hbhbh

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,32 +1,7 @@
-const daynames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Aaturday",
-]
-
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "Agust",
-    "September",
-    "October",
-    "November",
-    "December"
-]
-
-const todasydate = new Date();
-const dayName = dayName[todasydate.getDay()];
-const monthName = months [getMonth()];
-const year = todasydate.getFullYear;
-const currentdate = dayName + ", "  +  todasydate.getDate() + " " + monthName +", " +todasydate.getFullYear();
-
-document.getElementBy('currentdate').textContent = currentdate;
+const options = {
+    weeklday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options)
