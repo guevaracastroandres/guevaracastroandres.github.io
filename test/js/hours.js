44084efe9f4fe95595caab5712d74bd6b502fe0f
guevaracastroandres <cas18057@byui.edu>
2019-10-12 22:48:09 -0500

    todo

--- test/js/hours.js
+++ test/js/hours.js
@@ -1,7 +1,8 @@
 const options = {
-    weeklday: 'long',
+    weekday: 'long',
     day: 'numeric',
     month: 'long',
     year: 'numeric'
 };
 document.getElementById('hours').textContent = new Date().toLocaleDateString('en-US', options)
+
